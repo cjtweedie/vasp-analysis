b2775417eec291eeec6cc166bb51523154541cfa
Connor <ctweedie98@gmail.com>
2025-11-19 11:39:07 +1000

    QoL improvements to analysis script

--- analysis.sh
+++ analysis.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-# /path/to/output_files
+# /path/to/inputs and /path/to/outputs
 # change this to match your directory structure
-dir=./test_data
+dir_in=./test_data
+dir_out=./test_outputs
 
-rm relax_energy.txt
+# create empty output files
 touch relax_energy.txt
-rm relax_force.txt
 touch relax_force.txt
 
 # get no. ions by difference between first/last line in total force list
-first=`grep -A 2 -n "POSITION" $dir/OUTCAR | tail -n +3 | head -n 1 | awk -F: '{printf "%i", $1}'`
-last=`grep -B 2 -n "total drift:" $dir/OUTCAR | head -n 1 | awk -F: '{printf "%i", $1}'`
+first=`grep -A 2 -n "POSITION" $dir_in/OUTCAR | tail -n +3 | head -n 1 | awk -F: '{printf "%i", $1}'`
+last=`grep -B 2 -n "total drift:" $dir_in/OUTCAR | head -n 1 | awk -F: '{printf "%i", $1}'`
 Nion=$((last-first+1))
 
 # grep no. ionic steps for plotting
-Nstep=`grep "Ionic step" $dir/OUTCAR | tail -n 1 | awk '{printf "%i", $4}'`
+Nstep=`grep "Ionic step" $dir_in/OUTCAR | tail -n 1 | awk '{printf "%i", $4}'`
 
 # for loop to get data at each ionic step
 # grep total energy at end of each ionic step
@@ -23,7 +23,7 @@ Nstep=`grep "Ionic step" $dir/OUTCAR | tail -n 1 | awk '{printf "%i", $4}'`
 for ((j=1; j<=$Nstep; j++))
 do
 	k=$((Nstep-j+1))
-	energy=`grep "free  energy" $dir/OUTCAR | tail -n $k | head -n 1 | awk '{print $5}'`
+	energy=`grep "free  energy" $dir_in/OUTCAR | tail -n $k | head -n 1 | awk '{print $5}'`
 	echo $j $energy >> relax_energy.txt
 done
 
@@ -41,10 +41,12 @@ awk -v Nion="$Nion" -v count="$count" '/TOTAL-FORCE/ {
 			max = Ftot
 	}
 	printf "%i %.5f\n", count, max >> "relax_force.txt"
-}' $dir/OUTCAR
+}' $dir_in/OUTCAR
 
 # plotting energy & forces as functions of relaxation step no.
 gnuplot ./converge_plot.gnu
 
-#gnuplot -e 'set terminal pngcairo; set output "energy_convergence.png"; set xlabel "ionic step no."; plot "relax_energy.txt" title "energy [eV]" with lines'
-#gnuplot -e 'set terminal pngcairo; set output "force_convergence.png"; set xlabel "ionic step no."; plot "relax_force.txt" title "force [eV/A]" with lines'
+# move outputs into output directory
+mv relax_energy.txt $dir_out
+mv relax_force.txt $dir_out
+mv fe_convergence.png $dir_out
